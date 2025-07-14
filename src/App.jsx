62e390438f424e56337c781b41920a7a43593e3b
mcamilamp <camimerpa@gmail.com>
2025-07-14 16:23:56 -0500

    Fixing the little scroll in the techSlider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
+import TechSlider from "./components/TechSlider";
 
 function App() {
   return (
     <>
       <NavBar />
       <Home />
+      <TechSlider />
     </>
   );
 }
---
+++ src/components/TechSlider.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+import "../assets/styles/TechSlider.css"; // Ensure you have the correct path to your CSS file
+import {
+  SiReact,
+  SiTailwindcss,
+  SiSpringboot,
+  SiPostgresql,
+  SiJavascript,
+  SiCss3,
+  SiHtml5,
+  SiMysql,
+  SiPostman,
+  SiGithub,
+  SiGit,
+  SiAngular,
+  SiPhp,
+  SiFigma,
+} from "react-icons/si";
+
+const TechSlider = () => {
+  return (
+    <div className="tech-slider-wrapper">
+      <div className="tech-slider">
+        <Marquee speed={50} gradient={false}>
+          <div className="tech-item react">
+            <SiReact />
+          </div>
+          <div className="tech-item tailwind">
+            <SiTailwindcss />
+          </div>
+          <div className="tech-item springboot">
+            <SiSpringboot />
+          </div>
+          <div className="tech-item postgresql">
+            <SiPostgresql />
+          </div>
+          <div className="tech-item js">
+            <SiJavascript />
+          </div>
+          <div className="tech-item css">
+            <SiCss3 />
+          </div>
+          <div className="tech-item html">
+            <SiHtml5 />
+          </div>
+          <div className="tech-item mysql">
+            <SiMysql />
+          </div>
+          <div className="tech-item postman">
+            <SiPostman />
+          </div>
+          <div className="tech-item github">
+            <SiGithub />
+          </div>
+          <div className="tech-item git">
+            <SiGit />
+          </div>
+          <div className="tech-item angular">
+            <SiAngular />
+          </div>
+          <div className="tech-item php">
+            <SiPhp />
+          </div>
+          <div className="tech-item figma">
+            <SiFigma />
+          </div>
+        </Marquee>
+      </div>
+    </div>
+  );
+};
+
+export default TechSlider;
